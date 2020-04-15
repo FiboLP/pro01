66441ffd4d9eb194795441ad9f6ddd2e0add3a49
ureact <ureact1@ReactLab.localhost>
2020-04-16 00:33:22 +0300

    One our commit

---
+++ step1/script.js
@@ -0,0 +1,44 @@
+"usr strict";
+/*
+var leftBorderWidth = 1;
+let second = 2;
+//const pi = 3,14;
+
+console.log(econd);*/
+
+// var number = 5;
+// var string = "Hello";
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+// var sym = Symbol();
+
+// // let persone = { // var obj = {};
+// //     name: "Jj",
+// //     age: 35,
+// //     isMarriad: false
+// // };
+// //let famaly1 = ['jj', 'p.jpg', 'orange.bmp', 'self.png']; //var obg = [];
+
+// console.log(famaly1[0]);
+
+// console.log(typeof("rrr" + 2222));
+
+// alert("Hello Vl")
+
+// confirm("Are you here?");
+
+//let answer = +prompt("Чи є вам 18?", "Так");
+//console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2); // 8:42-6 video ... Operators
+
+
